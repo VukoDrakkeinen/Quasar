eadb0e1b123f443fd136f9210d796c5646784921
VukoDrakkeinen <sneyk8+git@gmail.com>
2015-07-05 23:35:25 +0200

    Improved error message creation in ParseErr

--- src/quasar/qutils/qerr/parse.go
+++ src/quasar/qutils/qerr/parse.go
@@ -1,5 +1,7 @@
 package qerr
 
+import "fmt"
+
 type parseErr struct {
 	chainedErr
 	input string
@@ -9,6 +11,10 @@ func NewParse(msg string, parent error, input string) error {
 	return &parseErr{chainedErr: chainedErr{msg: msg, parent: parent}, input: input}
 }
 
-func (this *parseErr) Input() string {
+func (this *parseErr) Input() string { //TODO: this can't be used, because parseErr is unexported
 	return this.input
+}
+
+func (this parseErr) Error() string {
+	return fmt.Sprintf("%s (input: %s)\n  caused by: %s", this.msg, this.input, this.parent)
 }
